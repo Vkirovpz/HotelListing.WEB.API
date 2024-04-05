478271313f41b5d0a14d4dad96dc50d63de06ceb
Vkirovpz <Vkirovpz@students.softuni.bg>
2024-04-05 15:35:22 +0300

    Added Entity Framework and Created Database

---
+++ Data/HotelListingDbContext.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.WEB_API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Bulgaria",
+                    ShortName = "BG"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Italy",
+                    ShortName = "IT"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "France",
+                    ShortName = "FR"
+                });
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Victoria Palace",
+                    Address = "Sunny Beach",
+                    CountryId = 1,
+                    Rating = 5.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "CR7",
+                    Address = "Monaco",
+                    CountryId = 2,
+                    Rating = 7.0
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Paris Palace",
+                    Address = "Paris",
+                    CountryId = 3,
+                    Rating = 6.3
+                });
+        }
+    }
+}
---
+++ Migrations/20240405123033_SeedCountriesAndHotels.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListing.WEB_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCountriesAndHotels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1, "Bulgaria", "BG" },
+                    { 2, "Italy", "IT" },
+                    { 3, "France", "FR" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Sunny Beach", 1, "Victoria Palace", 5.5 },
+                    { 2, "Monaco", 2, "CR7", 7.0 },
+                    { 3, "Paris", 3, "Paris Palace", 6.2999999999999998 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
